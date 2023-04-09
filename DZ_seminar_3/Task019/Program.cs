bf3d730bd3a6bbf7afea94c73c2621ca349a2236
igorbaloshin <iebaloshin@gmail.com>
2023-04-09 09:52:51 +0300

    перемещено в общую папку

---
+++ DZ_seminar_3/Task019/Program.cs
@@ -0,0 +1,82 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.Clear();
+//-----------------------------------------------------
+int CountNumbers(int n)
+{
+    int count = 0;
+    if(n < 0)
+       n *= (-1); // n = n*(-1)
+    while(n > 0)
+    {
+        n /= 10; // n = n / 10
+        count++;
+
+    }
+    return count;
+} 
+//--------------------------------------------------------
+void FillArray(int[] array, int K)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index] = (K % Convert.ToInt32(Math.Pow(10, length - index)) - K % Convert.ToInt32(Math.Pow(10, (length - index - 1)))) / (Convert.ToInt32(Math.Pow(10, length - index - 1)));
+        index++;
+    }
+}
+//---------------------------------------------------------------Проверочный цикл
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+
+
+}
+
+//---------------------------------------------------------------------
+
+bool CheckArray(int[]array)
+{
+    int length = array.Length;
+    int index = 0;
+    //bool check = false;
+    while(index <= length/2)
+    {
+        if(array[index] != array[length-1-index])
+        return false;
+        index++;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите число : ");
+int Palindrom = int.Parse(Console.ReadLine());
+
+int N = CountNumbers(Palindrom);
+
+int[]palindrom = new int[N];
+
+FillArray(palindrom, Palindrom);
+
+//PrintArray(polindrom);
+
+bool Ch = CheckArray(palindrom);
+
+if(Ch == false)
+Console.WriteLine("Число не палиндром");
+if(Ch == true)
+Console.WriteLine("Число палиндром");
+
---
+++ DZ_seminar_9/Task064/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+
+
+string NumbersRec( int n)
+{
+    if(n >= 1) 
+    return
+    $"{n} " + NumbersRec(n - 1);
+    else return String.Empty;
+}
+
+WriteLine(NumbersRec(n));
